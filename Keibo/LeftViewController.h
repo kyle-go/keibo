162cdf931e3ca20173b246a2c54c70389b0c060e
wangkangan <kyle.script@gmail.com>
2013-11-01 14:02:20 +0800

    left view ui

--- Keibo/LeftViewController.h
+++ Keibo/LeftViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LeftViewController : UIViewController
+@interface LeftViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
--- Keibo/LeftViewController.m
+++ Keibo/LeftViewController.m
@@ -12,13 +12,19 @@
 
 @end
 
-@implementation LeftViewController
+@implementation LeftViewController {
+    NSArray *defaultSectionNames;
+    NSArray *sectionNames;
+}
+
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        defaultSectionNames = [[NSArray alloc] initWithObjects: @"全部微博", @"私信", @"提到我", @"评论", @"收藏", @"搜索", nil];
+        sectionNames = [[NSArray alloc] initWithObjects:@"", @"分组", @"话题", nil];
     }
     return self;
 }
@@ -33,6 +39,60 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -- table View Data Source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return [sectionNames count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return [defaultSectionNames count];
+    } else if (section == 1) {
+        return 1;
+    } else if (section == 2) {
+        return 1;
+    }
+    return 0;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    return [sectionNames objectAtIndex:section];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"left-view-identifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    if (indexPath.section == 0) {
+        cell.textLabel.text = [defaultSectionNames objectAtIndex:indexPath.row];
+    } else if (indexPath.section == 1) {
+        cell.textLabel.text = @"互粉微博";
+    } else if (indexPath.section == 2) {
+       cell.textLabel.text = @"热门话题";
+    }
+    
+    return cell;
+}
+
+
+//设置section头部
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 0.001;
+    }
+    return 30;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.001;
 }
 
 @end
