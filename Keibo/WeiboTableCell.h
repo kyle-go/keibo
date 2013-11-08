ebfb86a35a389aca50e65c912c7d0694a04389be
wangkangan <kyle.script@gmail.com>
2013-11-08 18:25:33 +0800

    添加 weibo cell

---
+++ Keibo/WeiboTableCell.h
@@ -0,0 +1,13 @@
+//
+//  WeiboTableCell.h
+//  Keibo
+//
+//  Created by kyle on 11/8/13.
+//  Copyright (c) 2013 kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeiboTableCell : UITableViewCell
+
+@end
