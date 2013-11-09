61f916394f30f9af5df00a2a5da897b068d6f8b9
kyle <kyle.script@gmail.com>
2013-11-09 23:22:02 +0800

    update struct of weibocelldata

--- Keibo/WeiboCellData.h
+++ Keibo/WeiboCellData.h
@@ -10,15 +10,24 @@
 
 @interface WeiboCellData : NSObject
 
-@property (assign, nonatomic)NSInteger weiboId;
-@property (strong, nonatomic)NSString *avatarUrl;
-@property (strong, nonatomic)NSString *name;
-@property (strong, nonatomic)NSString *nickName;
-@property (strong, nonatomic)NSString *feedComeFrom;
-@property (strong, nonatomic)NSDate *date;
+@property (assign, nonatomic)NSInteger weiboId;         //微博唯一标识
+@property (strong, nonatomic)NSString *avatarUrl;       //头像http url
+@property (strong, nonatomic)NSString *name;            //微博发送者
+@property (strong, nonatomic)NSString *remarkName;      //备注名，暂时不会用到
+@property (strong, nonatomic)NSString *feedComeFrom;    //来自哪
+@property (strong, nonatomic)NSString *content;         //微博内容
+@property (strong, nonatomic)NSDate *date;              //微博发生日期
+@property (assign, nonatomic)NSInteger reposts;         //转发数
+@property (assign, nonatomic)NSInteger comments;        //评论数
+@property (assign, nonatomic)NSInteger likes;           // 赞数
 
-@property (assign, nonatomic)NSInteger reposts; //转发数
-@property (assign, nonatomic)NSInteger comments; //评论数
-@property (assign, nonatomic)NSInteger likes; // 赞数
+@property (assign, nonatomic)BOOL hasOriginWeibo;       //是否转发
+
+@property (assign, nonatomic)NSInteger originWeiboId;   //原微博唯一标识
+@property (strong, nonatomic)NSString *originName;      //原微博发送者
+@property (strong, nonatomic)NSString *originContent;   //原微博内容
+@property (assign, nonatomic)NSInteger originReposts;   //转发数
+@property (assign, nonatomic)NSInteger originComments;  //评论数
+@property (assign, nonatomic)NSInteger originLikes;     // 赞数
 
 @end
