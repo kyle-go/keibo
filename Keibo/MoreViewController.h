1079dbed34006f3fecc7dd6c857f4b02cfa3508e
kyle <kyle.script@gmail.com>
2013-11-25 00:33:41 +0800

    setting view

--- Keibo/MoreViewController.h
+++ Keibo/MoreViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MoreViewController : UIViewController
+@interface MoreViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
--- Keibo/MoreViewController.m
+++ Keibo/MoreViewController.m
@@ -36,4 +36,76 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Table view data source
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return 1;
+        case 1:
+            return 2;
+        case 2:
+            return 1;
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *commonIdentifier = @"commonIdentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:commonIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:commonIdentifier];
+    }
+    NSString *text;
+    switch (indexPath.section) {
+        case 0:
+            text = @"草稿箱";
+            break;
+        case 1:
+            if (indexPath.row == 0) {
+                text = @"清除缓存";
+            } else {
+                text = @"关于小果微博";
+            }
+            break;
+        case 2:
+            text = @"退出当前账号";
+            break;
+        default:
+            break;
+    }
+    cell.textLabel.text = text;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 20.0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.001;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 36.0;
+}
+
 @end
